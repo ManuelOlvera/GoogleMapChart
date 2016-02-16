9b67165bfaf5d6445e944ca94026bfdc7b9cd769
Manuel Olvera <manuel.olvera@makepositive.com>
2016-02-16 15:04:59 +0000

    playing with jasmine

--- spec/loginSpec.js
+++ spec/loginSpec.js
@@ -2,26 +2,43 @@ var request = require('request');
 var googleMapChart = require("../app.js");
 var base_url = 'http://localhost:3000/';
 
-describe("Login API", function() {
-  describe("GET /", function() {
-    it("returns status code 200", function(done) {
-      request.get(base_url, function(error, response, body) {
-        //console.log('base_url', base_url);
-        //console.log('error', error);
-        console.log('response', response.request.uri);
-        //console.log('body', body);
-        expect(response.statusCode).toBe(200);
-        done();
-      });
+describe("long asynchronous specs", function() {
+  
+  it('register process test', function(done) {
+    request.post({
+      url: base_url + 'register',
+      json: true,
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ username: 'test@test.com', password: '123451', confirmPassword: '12345' })
+    }, function(error, response, body) {
+      console.log('error', error);
+      console.log('body', body);
+      expect(response.statusCode).toBe(302);
+      done();
     });
+  });
+  
+  it('returns user to login page', function(done) {
+    request.get(base_url, function(error, response, body) {
+      //console.log('base_url', base_url);
+      //console.log('error', error);
+      console.log('response', response.request.uri);
+      //console.log('body', body);
+      expect(response.statusCode).toBe(200);
+      done();
+    });
+  });
 
-    // do I need a close server function for testing?
-    /*it("returns Hello World", function(done) {
-      request.get(base_url, function(error, response, body) {
-        // expect(body).toBe("Hello World");
-        // googleMapChart.closeServer();
-        done();
-      });
-    });*/
+  it('returns user to login page', function(done) {
+    request.get(base_url + 'logout', function(error, response, body) {
+      //console.log('base_url', base_url);
+      //console.log('error', error);
+      console.log('response', response.request.uri);
+      //console.log('body', body);
+      expect(response.statusCode).toBe(200);
+      done();
+    });
   });
 });
